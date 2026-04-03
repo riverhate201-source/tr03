7625a7b4cc400a38b00c172b1ce7a977f3053fcc
김예은 <riverhate201@gmail.com>
2026-04-03 15:38:15 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,18 @@
 // 자바스크립트를 위에 넣어도 쓸 수 있게 하는 방법
 $(function () {
     lucide.createIcons();
+
+    const swiper = new Swiper('.main_visual_slider', {
+        loop: true,
+        autoplay: {
+            delay: 3000, // 3초마다 슬라이드
+            disableOnInteraction: false, // 클릭해도 계속 자동재생
+        },
+
+        effect: 'fade', // ⭐ 페이드 효과
+
+        fadeEffect: {
+            crossFade: true // 자연스럽게 겹치면서 전환
+        },
+    });
 })
